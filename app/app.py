d35cfdd059022b065d22227c2a9acecdbb4a72ab
ASchneidman <alexschneidman@Alexs-MacBook-Pro.local>
2018-02-09 19:05:12 -0500

    website files

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, jsonify, send_from_directory
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def main():
+    app.run(host='0.0.0.0')
+
+if __name__ == '__main__':
+    main()
