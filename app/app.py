1c2fcce968cc5d7959e9c34e3be4f90738d7c972
ASchneidman <alexschneidman@Alexs-MacBook-Pro.local>
2018-02-10 00:37:54 -0500

    afsd

--- app/app.py
+++ app/app.py
@@ -15,23 +15,16 @@ def get_path(coords):
     result = get_tweets(arr[0], arr[1], 20)
     result1 = []
     print(str(result))
-    bycount = 0
     for tweet in result:
         tweeter = tweet.split(' ')
         if "by" in tweeter:
-            bycount += 1
             result1.append(re.sub('stco*','',re.sub('http*','',re.sub('[^A-Za-z0-9]+','',tweeter[tweeter.index("by")+1]))))
-<<<<<<< HEAD
-            print (bycount)
-            #if tweeter.index("by")+2 < len(tweeter):
-                #result1[len(result1)-1] += ' ' + re.sub('stco*','',re.sub('http*','',re.sub('[^A-Za-z0-9]+','',tweeter[tweeter.index("by")+2])))
-=======
 
         elif "#NowPlaying" in tweeter:
             result1.append(re.sub('stco*','',re.sub('http*','',re.sub('[^A-Za-z0-9]+','',tweeter[tweeter.index("#NowPlaying")+1]))))
             if tweeter.index("#NowPlaying")+2 < len(tweeter):
                 result1[len(result1)-1] += ' ' + re.sub('stco*','',re.sub('http*','',re.sub('[^A-Za-z0-9]+','',tweeter[tweeter.index("#NowPlaying")+2])))
->>>>>>> f584316e96497eff8875fedbd6e864e781d67bee
+
     result1 = list(set(result1))
             #result1.append(tweeter[tweeter.index("by")+2])
     return (str(result1))
