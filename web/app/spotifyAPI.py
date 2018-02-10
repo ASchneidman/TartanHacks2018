2b1b45c674e6c9d3b61d9ae4f17b793985f5a3bd
ASchneidman <alexschneidman@Alexs-MacBook-Pro.local>
2018-02-10 11:02:05 -0500

    asdf

--- web/app/spotifyAPI.py
+++ web/app/spotifyAPI.py
@@ -5,13 +5,18 @@ import spotipy.util as util
 def get_tracks(queries=["Post Malone"]):
     ids = []
     scope = 'playlist-modify-public'
-    token = util.prompt_for_user_token(username="ASchneidman",scope=scope,client_id='7170451d1ae941d6967a9976a375cd00',client_secret='bc9a015ae6ce4f119d514b684998e084',redirect_uri='http://localhost/')
+    username = "schneidmans"
+    playlist_name = "Local"
+    playlist_desc = "Local"
+    token = util.prompt_for_user_token(username=username,scope=scope,client_id='7170451d1ae941d6967a9976a375cd00',client_secret='bc9a015ae6ce4f119d514b684998e084',redirect_uri='http://localhost/')
     sp = spotipy.Spotify(auth=token)
     for query in queries:
         print(query)
         results = sp.search(q='artist:{}'.format(query), limit=1, type='artist')
         items = results['artists']['items']
         if (len(items) != 0):
+            if (len(ids) > 10):
+                break
             res = items[0]['id']
             ids.append(res)
             print (res)
@@ -19,14 +24,21 @@ def get_tracks(queries=["Post Malone"]):
     tracks = []
     track_ids = []
     for id in ids:
+        if (len(track_ids) > 10):
+            break
         trackJSON = sp.artist_top_tracks(id)
         tracks_list = trackJSON['tracks']
         for i in tracks_list:
             tracks.append(i['name'])
             track_ids.append(i['id'])
-        print(tracks[-1])
-        print(track_ids[-1])
-    return ids
+    userid = 'C1pjRNm-QfSxWdfYr1BhDg'
+
+    sp.trace = False
+    playlist = sp.user_playlist_create(username, playlist_name)
+
+    playlistid = playlist['id']
+    results = sp.user_playlist_add_tracks(username, playlistid, track_ids)
+    return playlistid
 
 
 
